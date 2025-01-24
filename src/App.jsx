936a79f92832818babc1fa4bfb9790b3635bcf8e
Ranjan Gupta <ranjanrider550@gmail.com>
2025-01-24 12:02:16 +0530

    initial repo setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='bg-slate-500'>gcs </p>
+    </>
+  )
+}
+
+export default App
